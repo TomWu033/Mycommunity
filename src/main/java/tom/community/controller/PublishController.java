2f6d9180c59029f03b6e73d5e1f611668be0ea7c
TomWu033 <wumeng123@sjtu.edu.cn>
2019-07-01 21:12:28 +0800

    tag

--- src/main/java/tom/community/controller/PublishController.java
+++ src/main/java/tom/community/controller/PublishController.java
@@ -1,5 +1,6 @@
 package tom.community.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import tom.community.cache.TagCache;
 import tom.community.dto.QuestionDTO;
 import tom.community.mapper.QuestionMapper;
 import tom.community.model.Question;
@@ -33,11 +35,13 @@ public class PublishController {
         model.addAttribute("description",question.getDescription());
         model.addAttribute("tag",question.getTag());
         model.addAttribute("id",question.getId());
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
     @GetMapping("/publish")
-    public String publish(){
+    public String publish(Model model){
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
@@ -52,6 +56,7 @@ public class PublishController {
         model.addAttribute("title",title);
         model.addAttribute("description",description);
         model.addAttribute("tag",tag);
+        model.addAttribute("tags", TagCache.get());
 
         User user = (User)request.getSession().getAttribute("user");
         if (user==null){
@@ -71,6 +76,12 @@ public class PublishController {
             model.addAttribute("error","标签不能为空");
             return "publish";
         }
+        String invalid = TagCache.filterInvalid(tag);
+        if (StringUtils.isNotBlank(invalid)){
+            model.addAttribute("error","输入非法标签"+invalid);
+            return "publish";
+        }
+
         Question question=new Question();
         question.setTitle(title);
         question.setDescription(description);
